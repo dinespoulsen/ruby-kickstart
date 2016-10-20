aee01ca17cb5d0f5ade59512877fb6f3d0822d5e
Dines Poulsen <dinespoulsen84@gmail.com>
2016-10-20 16:54:08 +0200

    finished challenges 1

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -5,4 +5,17 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+	letters = string.split("")
+
+	word = []
+	i = 1
+	letters.each do |element|
+		if element == "r" || element == "R"
+			word.push(letters[i])	
+		end
+	i +=1
+	end
+	return word.join("")
 end
+
+puts pirates_say_arrrrrrrrr("are you really learning Ruby?")
