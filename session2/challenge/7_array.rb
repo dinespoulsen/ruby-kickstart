febff105a827068266a7648a15a9a10f85a695b0
Dines Poulsen <dinespoulsen84@gmail.com>
2016-10-21 10:11:04 +0200

    finishing exercise 6 in challenges 2

--- session2/challenge/7_array.rb
+++ session2/challenge/7_array.rb
@@ -8,3 +8,17 @@
 # alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+
+def alternate_words(sentence)
+	'!@$#%^&*()-=_+[]:;,./<>?\\|'.split(//).each do |char|
+    sentence = sentence.gsub(char, ' ')
+  end
+
+	words = sentence.split
+	puts words
+	second_word = words.select.each_with_index {|element, i| i.even?}
+
+end
+
+
+alternate_words("hej med dig?")
