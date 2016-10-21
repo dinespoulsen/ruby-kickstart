27e48e6a027add690e0b7164c68b1759275aa832
Dines Poulsen <dinespoulsen84@gmail.com>
2016-10-21 17:19:17 +0200

    Added hash that counts number of words

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,18 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+
+def word_count(string)
+	hash = Hash.new { 0 }
+	return hash if string == ""
+
+	words = string.downcase.split(" ")
+	hash = Hash.new { 0 }
+	words.each {|word| hash[word] = words.count(word) }
+
+	return hash
+end
+
+
+word_count("the dog and the cat")
